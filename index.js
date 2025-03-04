e90cadf73a6dcf8d5f48e3fcdcd7d1216874bf40
Konstantin Kolesnikov <kolesnikov.kons@gmail.com>
2025-03-04 16:00:00 +0500

    Add 3 and 4

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
-const {readLine} = require('./console');
+const { getAllFilePathsWithExtension, readFile } = require('./fileSystem');
+const { readLine } = require('./console');
 
 const files = getFiles();
 
@@ -11,23 +11,44 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function extractTodos() {
+    const todos = [];
+    files.forEach(file => {
+        const lines = file.split('\n');
+        lines.forEach(line => {
+            const match = line.match(/\/\/ TODO (.*)/);
+            if (match) {
+                todos.push(match[1]);
+            }
+        });
+    });
+    return todos;
+}
+
 function processCommand(command) {
-    switch (command) {
+    const todos = extractTodos();
+    const args = command.split(' ');
+
+    switch (args[0]) {
         case 'exit':
             process.exit(0);
             break;
         case 'show':
-            const todos = [];
-            files.forEach(file => {
-                const lines = file.split('\n');
-                lines.forEach(line => {
-                    let splited = line.split('// TODO ');
-                    if (splited.length > 1) {
-                        todos.push(splited.slice(1).join(''));
-                    }
-                });
-            });
             console.log(todos);
+            break;
+        case 'important':
+            console.log(todos.filter(todo => todo.includes('!')));
+            break;
+        case 'user':
+            if (args.length < 2) {
+                console.log('Please provide a username');
+                break;
+            }
+            const username = args[1].toLowerCase();
+            console.log(todos.filter(todo => {
+                const match = todo.match(/^([^;]+);/);
+                return match && match[1].toLowerCase() === username;
+            }));
             break;
         default:
             console.log('wrong command');
